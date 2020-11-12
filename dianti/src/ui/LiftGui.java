83022f8dcf8d93b9fb25e9e3781075d74c2ca0c1
lhj <1457111261@qq.com>
2020-11-12 20:35:07 +0800

    commit three

--- dianti/src/ui/LiftGui.java
+++ dianti/src/ui/LiftGui.java
@@ -26,9 +26,12 @@ public class LiftGui extends JFrame implements ActionListener{
 	final private int WAITTIMERUN = 1;
 	
 	Color background;
+	Color textBackground;
 	
 	// 等待时间
 	private int waitTime = 0;
+	
+	private boolean waitOrRun;
 
 	// 文本框
 	private JTextField field;// 楼层文本框
@@ -36,7 +39,7 @@ public class LiftGui extends JFrame implements ActionListener{
 	private JTextField weightField;// 重量文本框
 
 	// 按钮
-	private JButton[] buttons1;//
+	private JButton[] buttons1;
 	private JButton buttonsOpen;
 	private JButton buttonsClose;
 	private JButton[] buttonUps;
@@ -47,9 +50,16 @@ public class LiftGui extends JFrame implements ActionListener{
 	private JPanel jp1;
 	private JPanel jp2;
 	
+	// 电梯当前重量
+	private int nowWeight;
+	
 	public LiftGui() throws Exception{
 		String[] name1={"1","2","3","4","5","6","7","8","9","10","11","12"};
-		String[] name2={"<>","><"};
+		// 图片对象
+		Icon imageUp = new ImageIcon("images/up.png");
+		Icon imageDown = new ImageIcon("images/down.png");
+		Icon imageOpen = new ImageIcon("images/open.png");
+		Icon imageClose = new ImageIcon("images/close.png");
 
 		this.setTitle("电梯");
 		this.setLocation(300, 200);
@@ -82,12 +92,11 @@ public class LiftGui extends JFrame implements ActionListener{
 			buttons1[i].addActionListener(this);
 		}
 		// 开关电梯按钮
-		buttonsOpen =new JButton(name2[0]);
+		buttonsOpen =new JButton(imageOpen);
 		buttonsOpen.setSize(60,40);
 		buttonsOpen.addActionListener(this);
-		background = buttonsOpen.getBackground();
 		jp1.add(buttonsOpen);
-		buttonsClose =new JButton(name2[1]);
+		buttonsClose =new JButton(imageClose);
 		buttonsClose.setSize(60,40);
 		buttonsClose.addActionListener(this);
 		jp1.add(buttonsClose);
@@ -96,9 +105,7 @@ public class LiftGui extends JFrame implements ActionListener{
 		jp2.setLayout(new GridLayout(12,2,5,5));
 		jp2.setBounds(320,160,120,500);
 		
-		// 图片对象
-		Icon imageUp = new ImageIcon("images/up.jpg");
-		Icon imageDown = new ImageIcon("images/down.jpg");
+		// 将每一层的按钮都加入到面板二
 		for(int i=12;i>=1;i--){
 			buttonUps[i] = new JButton(imageUp);
 			buttonUps[i].setToolTipText(i+"");
@@ -121,8 +128,7 @@ public class LiftGui extends JFrame implements ActionListener{
 		field.setBackground(Color.WHITE);
 		timeField.setBackground(Color.WHITE);
 		weightField.setBackground(Color.white);
-//		jp.setBackground(Color.green);
-//		jp1.setBackground(Color.black);
+
 		field.setHorizontalAlignment(JTextField.CENTER);
 		timeField.setHorizontalAlignment(JTextField.CENTER);
 		weightField.setHorizontalAlignment(JTextField.CENTER);
@@ -133,12 +139,20 @@ public class LiftGui extends JFrame implements ActionListener{
 		field.setFont(font1);
 		timeField.setFont(fontTime);
 		weightField.setFont(fontTime);
+		
+		// 设置显示楼层文本框的默认颜色
+		field.setBackground(Color.decode("#cde6c7"));
 
 		field.setEditable(false);//使文本框不可选中
 		timeField.setEditable(false);
 		weightField.setEditable(false);
+		
+		
+		// 获取按钮的默认颜色
+		background = buttonsOpen.getBackground();
+		textBackground = weightField.getBackground();
 
-		Container c=this.getContentPane();
+		Container c = this.getContentPane();
 		c.setLayout(null);
 		c.add(jp);
 		c.add(field);
@@ -148,49 +162,88 @@ public class LiftGui extends JFrame implements ActionListener{
 		c.add(weightField);
 		this.setVisible(true);
 		
-		// 线程
+		// 使用线程实现电梯等待效果
 		new Thread(new Runnable() {
 			
 			@Override
 			public void run() {
 				while(true) {
+					// 判断状态，确定是否到达指定的楼层，到达则将按钮弹起（改变颜色）
 					statusLift = RunLift.next(insideLift, outsideLift);
-					System.out.println(statusLift.isStatus());
-					if(insideLift.getUpOrDown() == InsideLift.Wait) {
+					
+//					System.out.println("状态-->"+waitOrRun);
+					
+					if(insideLift.getUpOrDown() == InsideLift.Wait){
 						insideLift.getBtn()[statusLift.getFloor()] = false;
 						outsideLift.getDown()[statusLift.getFloor()] = false;
 						outsideLift.getUp()[statusLift.getFloor()] = false;
 					}
+					
 					if(statusLift.isStatus() == StatusLift.WAIT) {
+						
 						if (insideLift.getBtn()[statusLift.getFloor()] == false)
 							buttons1[statusLift.getFloor()].setBackground(background); 
 						if (outsideLift.getUp()[statusLift.getFloor()] == false)
 							buttonUps[statusLift.getFloor()].setBackground(background); 
 						if (outsideLift.getDown()[statusLift.getFloor()] == false)
 							buttonDowns[statusLift.getFloor()].setBackground(background); 
+						
+						// 判断电梯是否还要继续工作
+						waitOrRun = true;
+						for(int i=1;i<=RunLift.MAXFLOOR;i++){
+							if(insideLift.getBtn()[i]||outsideLift.getUp()[i]||outsideLift.getDown()[i]){
+								waitOrRun = false;
+								break;
+							}
+						}
+						
+						// 重置等待时间
 						waitTime = WAITTIMEWAIT;
-						insideLift.setWeigth((int)(Math.random() * 1200));
-						// 重量显示
-						weightField.setText(insideLift.getWeigth() + "kg");
-						// 等待时间显示
-						timeField.setText(String.valueOf(waitTime));
+						// 获取随机体重
+						nowWeight = (int)(Math.random() * 1200);
+						if(waitOrRun){
+							timeField.setText("---");
+							weightField.setText("---");
+						}else{
+							// 若体重超过最大承重，修改颜色，否则，置为之前的颜色
+							if(nowWeight>RunLift.MAXWEIGHT) weightField.setBackground(Color.decode("#D71345"));
+							else weightField.setBackground(textBackground);
+							insideLift.setWeigth(nowWeight);
+							// 重量显示
+							weightField.setText(insideLift.getWeigth() + "kg");
+							// 时间显示
+							timeField.setText(String.valueOf(waitTime));
+						}
+						
 					}else {
-						waitTime = WAITTIMERUN;
-						timeField.setText(String.valueOf(waitTime));
+						if(waitOrRun){
+							timeField.setText("---");
+							weightField.setText("---");
+						}else{
+							// 时间显示
+							waitTime = WAITTIMERUN;
+							timeField.setText(String.valueOf(waitTime));
+//							// 重量显示
+//							insideLift.setWeigth(nowWeight);
+//							weightField.setText(insideLift.getWeigth() + "kg");
+						}
 					}
 					field.setText(String.valueOf(statusLift.getFloor()));
 					System.out.println(statusLift.getFloor());
 					try {
 						while(waitTime > 0) {
+							if(!waitOrRun) {
+								timeField.setText(String.valueOf(waitTime));
+								insideLift.setWeigth(nowWeight);
+								weightField.setText(insideLift.getWeigth() + "kg");
+							}
 							Thread.sleep(500);
 							waitTime--;
-							timeField.setText(String.valueOf(waitTime));
 						}
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-//					System.out.println("等待结束");
 				}
 				
 			}
@@ -216,14 +269,17 @@ public class LiftGui extends JFrame implements ActionListener{
 					if(event.getSource() == buttons1[i]) {
 						insideLift.getBtn()[i] = true;
 						buttons1[i].setBackground(Color.decode("#d3d7d4"));
+						dataInit();
 						break;
 					}else if(event.getSource() == buttonUps[i]) {
 						outsideLift.getUp()[i] = true;
 						buttonUps[i].setBackground(Color.decode("#d3d7d4"));
+						dataInit();
 						break;
 					}else if(event.getSource() == buttonDowns[i]) {
 						outsideLift.getDown()[i] = true;
 						buttonDowns[i].setBackground(Color.decode("#d3d7d4"));
+						dataInit();
 						break;
 					}
 				}
@@ -233,6 +289,19 @@ public class LiftGui extends JFrame implements ActionListener{
 			e.printStackTrace();
 		}
 		
+	}
+	
+	public void dataInit(){
+		if(waitOrRun&&statusLift.isStatus() == StatusLift.WAIT){
+			waitTime = WAITTIMEWAIT;
+			if(nowWeight>RunLift.MAXWEIGHT) weightField.setBackground(Color.decode("#D71345"));
+			else weightField.setBackground(textBackground);
+			insideLift.setWeigth(nowWeight);
+			// 重量显示
+			weightField.setText(insideLift.getWeigth() + "kg");
+			timeField.setText(String.valueOf(waitTime));
+			waitOrRun = false;
+		}
 	}
 	
 }
